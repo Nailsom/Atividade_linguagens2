ba2f873d28312915f7aad6637a6b681cbd596669
Nailson Onesio de Almeida <102481292+Nailsom@users.noreply.github.com>
2024-11-28 12:11:43 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Automovel carro = new Automovel("Toyota", 2020, 85000.00);
+        Automovel carro = new Automovel("Toyota", "Corolla", 2020, 85000.00);
 
         carro.exibirInformacoes();
 
         carro.setMarca("Honda");
+        carro.setModelo("Civic");
         carro.setAno(2022);
         carro.setValor(95000.00);
 
