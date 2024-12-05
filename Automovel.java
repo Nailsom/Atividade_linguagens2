6600bfe7dd633b5e5c17515194925f36de28a859
Nailson Onesio de Almeida <102481292+Nailsom@users.noreply.github.com>
2024-12-05 11:02:45 -0300

    Update Automovel.java

--- Automovel.java
+++ Automovel.java
@@ -15,33 +15,11 @@ public class Automovel {
         return marca;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
 
     public String getModelo() {
         return modelo;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
 
     public void exibirInformacoes() {
         System.out.println("Marca: " + marca);
